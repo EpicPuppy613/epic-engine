85b881ab66ea0be9fea0f9874a3a390cc8919e9e
EpicPuppy613 <epicpuppy@epicpuppy.dev>
2023-02-10 16:58:17 +0000

    Remove SemVer

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ import { EventBus } from "./modules/eventbus/eventbus.js";
 import { EventHandler } from "./modules/eventbus/eventhandler.js";
 import { EventType } from "./modules/eventbus/eventtype.js";
 import { Event } from "./modules/eventbus/event.js";
-import { SemVer } from "./modules/semver.js";
 
-export {SemVer, Event, EventBus, EventHandler, EventType};
+export {Event, EventBus, EventHandler, EventType};
